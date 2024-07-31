d23b2bc167e5b7bba7839ad978d99a400dd2cf7b
Rohan Tyagi <tyagirohan798@gmail.com>
2024-07-31 13:53:53 +0530

    MySql connection error bug fix.

--- MySQL.py
+++ MySQL.py
@@ -20,7 +20,7 @@ def Save_Data_MySql(B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R):
         command = "create table data(user int auto_increment key not null, Name varchar(50), Date varchar(100), DOB varchar(50), age varchar(100), sex varchar(100), Cp varchar(100), trestbps varchar(100), chol varchar(100), fbs varchar(100), restecg varchar(100), thalach varchar(100), exang varchar(100), oldpeak varchar(100), slope varchar(100), ca varchar(100), thal varchar(100), result varchar(100))"
         mycursor.execute(command)
 
-        command = "insert into data(Name, Date, DOB, age, sex, Cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal, Result) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        command = "insert into data(Name, Date, DOB, age, sex, Cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal, Result) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
         mycursor.execute(command, (B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R))
         mydb.commit()
         mydb.close()
@@ -28,10 +28,10 @@ def Save_Data_MySql(B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R):
 
     except:
         mycursor.execute("use heart_data")
-        mydb = mysql.connector.connect(host="localhost", user="root", password = "992922", database="Heart_Data")
+        mydb = mysql.connector.connect(host="localhost", user="root", password = "iltpbaf@007", database="Heart_Data")
         mycursor = mydb.cursor()
 
-        command = "insert into data(Name, Date, DOB, age, sex, Cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal, Result) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
+        command = "insert into data(Name, Date, DOB, age, sex, Cp, trestbps, chol, fbs, restecg, thalach, exang, oldpeak, slope, ca, thal, Result) values(%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)"
         mycursor.execute(command, (B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R))
         mydb.commit()
         mydb.close()
